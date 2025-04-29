95986b26a63612ee8d1d0b3af42432019b0aebb3
Bryan <bryanfinesw@gmail.com>
2025-04-28 22:35:10 -0400

    added signup/login/logout routes

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,35 @@
 const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const prisma = require("../utils/prisma.js");
 
-exports.home = (req, res) => {
-    res.send("Hello world");
-}
+exports.signup = async (req, res, next) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        await prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: hashedPassword,
+                email: req.body.email,
+            }
+        });
+
+        res.redirect("/login");
+    } catch (error) {
+        console.error("Error: " + error);
+        next(error);
+    }
+};
+
+exports.login = (req, res, next) => {
+    return passport.authenticate("local", {
+            successRedirect: "/",
+            failureRedirect: "/"
+        })(req, res, next);
+};
+
+exports.logout = (req, res, next) => {
+    req.logout((error) => {
+        if (error) return next(error);
+        res.redirect("/");
+    });
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,10 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require("../controllers/indexController.js");
 
-indexRouter.get("/sign-up", indexController.signUp);
+indexRouter.get("/sign-up", indexController.signup);
+indexRouter.get("/logout", indexController.logout);
+
+indexRouter.post("/sign-up", indexController.signup);
+indexRouter.post("/login", indexController.login);
 
 module.exports = indexRouter;
